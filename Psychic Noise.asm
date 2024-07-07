f568500787e3be56579720529bf36388af9d9be0
Gelius3 <110195781+Gelius3@users.noreply.github.com>
2024-07-08 00:20:24 +0100

    Created Psychic Noise

---
+++ Psychic Noise.asm
@@ -0,0 +1,94 @@
+; -------------------------------------------------------------------------
+; Gelius - 08/07/2024 - Psychic Noise
+; Psychic Noise deals Damage and prevents the target from healing. This move
+; is a Sound Based Move.
+; Based on Hecka-Bad's Boomburst.
+; -------------------------------------------------------------------------
+
+.relativeinclude on
+.nds
+.arm
+
+.definelabel MaxSize, 0x2598
+
+; For US (comment for EU)
+.definelabel MoveStartAddress, 0x02330134
+.definelabel MoveJumpAddress, 0x023326CC
+.definelabel DealDamage, 0x02332B20
+.definelabel DefenderAbilityIsActive, 0x022F96CC
+.definelabel SubstitutePlaceholderStringTags, 0x022E2AD8
+.definelabel LogMessageByIdWithPopupCheckUserTarget, 0x0234B350
+.definelabel TryInflictHealBlockStatus, 0x23185A8
+
+; For EU (uncomment for EU)
+;.definelabel MoveStartAddress, 0x02330B74
+;.definelabel MoveJumpAddress, 0x0233310C
+;.definelabel DealDamage, 0x02333560
+;.definelabel DefenderAbilityIsActive, 0x022FA0D8
+;.definelabel SubstitutePlaceholderStringTags, 0x022E3418
+;.definelabel LogMessageByIdWithPopupCheckUserTarget, 0x0234BF50
+;.definelabel TryInflictHealBlockStatus, 0x2319008
+
+; Constants
+.definelabel TRUE, 0x1
+.definelabel FALSE, 0x0
+.definelabel SOUNDPROOF_ABILITY_ID, 60 ; 0x3C
+.definelabel SOUNDPROOF_STR_ID, 3769 ; 0xEB9
+
+; File creation
+.create "./code_out.bin", 0x02330134 ; Change to 0x02330B74 for EU.
+    .org MoveStartAddress
+    .area MaxSize
+        sub sp,sp,#0x4
+        mov r10,FALSE
+        
+        ; There is a list of sound moves in the base game. A skypatch could
+        ; be added to add a move ID to the list of sound moves in game, but
+        ; this is easier for people to implement and doesn't require a
+        ; custom skypatch.
+        mov r0,r9
+        mov r1,r4
+        mov r2,SOUNDPROOF_ABILITY_ID
+        mov r3,TRUE
+        bl  DefenderAbilityIsActive
+        cmp r0,FALSE
+        beq not_blocked
+        
+        mov r0,#1
+        mov r1,r4
+        mov r2,#0
+        bl  SubstitutePlaceholderStringTags ; Sub Target
+        ldr r2,=SOUNDPROOF_STR_ID
+        mov r0,r9
+        mov r1,r4
+        bl  LogMessageByIdWithPopupCheckUserTarget
+        b   return
+        
+    not_blocked:
+        ; Damage the target.
+        str r7,[sp,#0x0]
+        mov r0,r9
+        mov r1,r4
+        mov r2,r8
+        mov r3,#0x100 ; 1.0x, Normal Damage *(See Note 1 Below)
+        bl  DealDamage
+        
+		movs r10,r0
+		beq return
+		
+		mov r0,r9
+		mov r1,r4
+		mov r2,#1
+		mov r3,#0
+		bl TryInflictHealBlockStatus
+
+    return:
+        add sp,sp,#0x4
+        b   MoveJumpAddress
+        .pool
+    .endarea
+.close
+
+; Note 1: The game uses the 8 bits at the end as a kind of fraction/decimal
+; where the denominator is 256. So in this context, 0x100 means
+; (256/256) = 1
